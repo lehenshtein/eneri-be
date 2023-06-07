f177c7f82592142eb015b0ba97307b2bb4809793
lehenshtein <lehenshtein@gmail.com>
2023-06-07 21:26:18 +0300

    Revert "Revert "feat(games): removing master from gamerequest, fixed avatars, changed logic for master apply""
    This reverts commit 0680625cf4e548c13422dde5f5fa5dae006e41a4.

--- src/apps/game-request/game-request.model.ts
+++ src/apps/game-request/game-request.model.ts
@@ -2,6 +2,7 @@ import mongoose, { Document, Schema } from 'mongoose';
 import { IUserModel } from '../user/user.models';
 import { IGameSystem } from '../../models/GameSystem.interface';
 import { ICity } from '../../models/City.interface';
+import { ITimestamp } from '../../models/timestamp.interface';
 
 export interface IGameRequest {
   master: Partial<IUserModel> | undefined;
@@ -23,7 +24,7 @@ export interface IGameRequest {
   bookedAmount: number;
 }
 
-export interface IGameRequestModel extends IGameRequest, Document {}
+export interface IGameRequestModel extends IGameRequest, ITimestamp, Document {}
 
 const GameRequestSchema: Schema = new Schema({
   master: { type: Schema.Types.ObjectId, required: false, ref: 'User' },
